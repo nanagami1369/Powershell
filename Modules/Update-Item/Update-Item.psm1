3c0dc00ea0a14036114c4be3298ac7331edcc508
nasi nanagami <nanagami1369@gmail.com>
2018-12-15 15:00:20 +0900

    touchコマンドもどきのモジュールを作成

---
+++ Modules/Update-Item/Update-Item.psm1
@@ -0,0 +1,14 @@
+function Update-Item {
+	foreach ($item in $args) {
+		if (Test-Path $item) {
+			# 存在する場合 (ファイルの更新日時を変更)
+			Set-ItemProperty -Path $item -Name LastWriteTime -Value $(Get-Date)
+		}
+		else {
+			# 存在しない場合 (ファイルを作成)
+			New-Item -Type File "$item"
+		}
+	}
+}
+New-Alias -Name touch -Value Update-Item
+Export-ModuleMember -Function Update-Item -Alias touch
