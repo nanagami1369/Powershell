df8b640c0b03a123b3b7e29fbfb4a5f0499e440c
nanagami1369 <nanagami1369@gmail.com>
2019-06-01 13:04:49 +0900

    Update-Item Modulをwsl-commanndsに統合

--- Modules/wsl-comannds/wsl-comannds.psm1
+++ Modules/wsl-comannds/wsl-comannds.psm1
@@ -5,8 +5,20 @@ New-Alias -Name fish -Value Start-fishShell
 function Get-AllItem {
 	Get-ChildItem -Force $args
 }
+function Update-Item {
+	foreach ($item in $args) {
+		if (Test-Path $item) {
+			# 存在する場合 (ファイルの更新日時を変更)
+			Set-ItemProperty -Path $item -Name LastWriteTime -Value $(Get-Date)
+		}
+		else {
+			# 存在しない場合 (ファイルを作成)
+			New-Item -Type File "$item"
+		}
+	}
+}
 New-Alias la Get-AllItem
 New-Alias ll Get-ChildItem
-
-Export-ModuleMember -Function Start-fishShell, Get-AllItem
-Export-ModuleMember -Alias fish, ll, la
+New-Alias -Name touch -Value Update-Item
+Export-ModuleMember -Function Start-fishShell, Get-AllItem, Update-Item
+Export-ModuleMember -Alias fish, ll, la, touch
