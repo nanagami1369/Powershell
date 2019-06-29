d456ef9d830b8ca85e988a76570d867b464fe327
nanagami1369 <nanagami1369@gmail.com>
2019-06-29 10:36:14 +0900

    上手く動かなかったので元に戻す

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,38 @@
-chcp 65001
-
-#起動時のフォルダーを取得
-Set-Variable -Scope "Global" -Option "Constant" -Name "StartFolder" -Value $PWD
-
 #起動時のエラー音を削除
-Set-PSReadlineOption -BellStyle	 None
-
-#おまじない
-Get-ChildItem $HOME\OneDrive\vscode\PowerShell\Modules | Import-Module
-
+Set-PSReadlineOption -BellStyle None
+#関数読み込み
+function Update-Item {
+	foreach ($item in $args) {
+		if (Test-Path $item) {
+			# 存在する場合 (ファイルの更新日時を変更)
+			Set-ItemProperty -Path $item -Name LastWriteTime -Value $(Get-Date)
+		}
+		else {
+			# 存在しない場合 (ファイルを作成)
+			New-Item -Type File "$item"
+		}
+	}
+}
+New-Alias -Name touch -Value Update-Item
+function Start-fishShell {
+	wsl fish $args
+}
+New-Alias -Name fish -Value Start-fishShell
+function Get-AllItem {
+	Get-ChildItem -Force $args
+}
+New-Alias la Get-AllItem
+New-Alias ll Get-ChildItem
+function Get-Env {
+	Get-ChildItem Env:*$args*
+}
+New-Alias -Name printenv -Value Get-Env
+#ESC文字の登録(文字色の変換に使う)zzglobal:Get-AllItem
+$ESC = [char]27
+#promptの修正
+function prompt() {
+	[string]$Prompt = Get-Location
+	"$ESC[32m" + ($Prompt.Replace($HOME, "~$ESC[32m")) + "$ESC[0m>"
+}
 #キーバインドをEmacs風に
 Set-PSReadLineOption -EditMode Emacs
-
-# 起動時のコメント
-Write-Host "Welcome to my PowerShell.exe" -ForegroundColor Blue
