e31b37a94c5352b34a76f1ce647ada73f5cc5975
nanagami1369 <nanagami1369@gmail.com>
2020-02-09 13:52:14 +0900

    touchコマンドのcmdlet化

---
+++ Cmdlet/touch/touch/Touch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+
+namespace Touch
+{
+    [CmdletBinding()]
+    [Cmdlet(VerbsData.Update, "Item")]
+    public class Touch : PSCmdlet
+    {
+        [Parameter(ValueFromPipeline = true, Position = 1, Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
+        public string Path { get; set; }
+
+        protected sealed override void ProcessRecord()
+        {
+                Do(Path);
+            
+        }
+
+        private void Do(string path)
+        {
+            if (!System.IO.Path.IsPathRooted(path))
+            {
+                path = System.IO.Path.Combine(SessionState.Path.CurrentLocation.Path, path);
+            }
+
+            var DirectoryExists = Directory.Exists(path);
+            var FileExists = File.Exists(path);
+            if (DirectoryExists)
+            {
+                Directory.SetLastWriteTime(path, DateTime.Now);
+                return;
+            }
+
+            if (FileExists)
+            {
+                File.SetLastWriteTime(path, DateTime.Now);
+                return;
+            }
+
+            var baseDirectory = System.IO.Path.GetDirectoryName(path);
+            var baseDirectoryExists = string.IsNullOrEmpty(baseDirectory) || Directory.Exists(baseDirectory);
+            if (baseDirectoryExists)
+            {
+                File.Create(path);
+                return;
+            }
+
+            throw new DirectoryNotFoundException("ベースディレクトリが存在していません");
+        }
+    }
+}
--- Modules/wsl-comannds/wsl-comannds.psm1
+++ Modules/wsl-comannds/wsl-comannds.psm1
@@ -5,18 +5,6 @@ New-Alias -Name fish -Value Start-fishShell
 function Get-AllItem {
 	Get-ChildItem -Force $args
 }
-function Update-Item {
-	foreach ($item in $args) {
-		if (Test-Path $item) {
-			# 存在する場合 (ファイルの更新日時を変更)
-			Set-ItemProperty -Path $item -Name LastWriteTime -Value $(Get-Date)
-		}
-		else {
-			# 存在しない場合 (ファイルを作成)
-			New-Item -Type File "$item"
-		}
-	}
-}
 function Get-Env {
 	Get-ChildItem Env:*$args*
 }
@@ -24,5 +12,5 @@ New-Alias la Get-AllItem
 New-Alias ll Get-ChildItem
 New-Alias -Name touch -Value Update-Item
 New-Alias -Name printenv -Value Get-Env
-Export-ModuleMember -Function Start-fishShell, Get-AllItem, Update-Item, Get-Env
-Export-ModuleMember -Alias fish, ll, la, touch, printenv
+Export-ModuleMember -Function Start-fishShell, Get-AllItem, Get-Env
+Export-ModuleMember -Alias fish, ll, la, printenv
