a5ab34d96baaad7716af466f3eb64877f33ac65a
nanagami1369 <nanagami1369@gmail.com>
2020-02-09 13:55:13 +0900

    いい感じにした。更新忘れ

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,26 +3,32 @@ chcp 65001
 #起動時のフォルダーを取得
 Set-Variable -Scope "Global" -Option "Constant" -Name "StartFolder" -Value $PWD
 
+#tmpフォルダーへアクセスしやすくする
+Set-Variable -Scope "Global" -Option "Constant" -Name "TMP" -Value $env:TMP
 #起動時のエラー音を削除
 Set-PSReadlineOption -BellStyle None
 
 #おまじない
-#ESC文字の登録(文字色の変換に使う)
-Import-Module New-ModuleSet
-Import-Module wsl-comannds
-Import-Module Get-StartFolder
-Import-Module Search-Location
-Import-Module posh-vagrant
-$ESC = [char]27
+Get-ChildItem (Join-Path $PSScriptRoot \Modules) | Import-Module
 
 #promptの修正
+$ESC = [char]27
+$ESCColor = [string]"[32m"
+$promptFront = [char]'>'
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	$ESCColor = [string]"[31m"
+	$promptFront = [char]'#'
+}
 function prompt() {
 	[string]$Prompt = Get-Location
-	"$ESC[32m" + ($Prompt.Replace($HOME, "~$ESC[32m")) + "$ESC[0m>"
+	"$ESC$ESCColor" + ($Prompt.Replace($HOME, "~$ESC$ESCColor")) + "$ESC[0m$promptFront"
 }
 
 #キーバインドをEmacs風に
 Set-PSReadLineOption -EditMode Emacs
 
+#補完をtabで出来るように
+Set-PSReadlineKeyHandler -Chord tab -Function MenuComplete
+
 # 起動時のコメント
-Write-Host "Welcome to my PowerShell.exe" -ForegroundColor Blue
+Write-Host "Welcome to my PowerShell"$Host.Version.ToString() -ForegroundColor Blue
