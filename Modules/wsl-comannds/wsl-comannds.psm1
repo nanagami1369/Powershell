1d830b5383eeff4af29b28363e6a6583d8320d9e
nanagami1369 <nanagami1369@gmail.com>
2020-08-21 11:35:52 +0900

    touchモジュールをバイナリーからスクリプトに戻した

--- Modules/wsl-comannds/wsl-comannds.psm1
+++ Modules/wsl-comannds/wsl-comannds.psm1
@@ -8,10 +8,35 @@ function Get-AllItem {
 function Get-Env {
 	Get-ChildItem Env:*$args*
 }
+function Update-Item {
+	[CmdletBinding()]
+	param(
+		[Parameter(
+			ValueFromPipeline = $true,
+			Position = 1,
+			ValueFromPipelineByPropertyName = $True,
+			Mandatory = $true
+		)]
+		[ValidateNotNullOrEmpty()]
+		$path
+	)
+
+	process {
+		if (Test-Path $path) {
+			# 存在する場合 (ファイルの更新日時を変更)
+			Set-ItemProperty -Path $path -Name LastWriteTime -Value $(Get-Date)
+		}
+		else {
+			# 存在しない場合 (ファイルを作成)
+			New-Item -Type File "$path"
+		}
+	}
+}
+
 New-Alias la Get-AllItem
 New-Alias ll Get-ChildItem
 New-Alias -Name touch -Value Update-Item
 New-Alias -Name printenv -Value Get-Env
 New-Alias  .. cd..
-Export-ModuleMember -Function Start-fishShell, Get-AllItem, Get-Env
-Export-ModuleMember -Alias fish, ll, la, printenv, ..
+Export-ModuleMember -Function Start-fishShell, Get-AllItem, Get-Env, Update-Item
+Export-ModuleMember -Alias fish, ll, la, printenv, .., touch
--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,14 +2,26 @@
 Set-PSReadlineOption -BellStyle None
 #関数読み込み
 function Update-Item {
-	foreach ($item in $args) {
-		if (Test-Path $item) {
+	[CmdletBinding()]
+	param(
+		[Parameter(
+			ValueFromPipeline = $true,
+			Position = 1,
+			ValueFromPipelineByPropertyName = $True,
+			Mandatory = $true
+		)]
+		[ValidateNotNullOrEmpty()]
+		$path
+	)
+
+	process {
+		if (Test-Path $path) {
 			# 存在する場合 (ファイルの更新日時を変更)
-			Set-ItemProperty -Path $item -Name LastWriteTime -Value $(Get-Date)
+			Set-ItemProperty -Path $path -Name LastWriteTime -Value $(Get-Date)
 		}
 		else {
 			# 存在しない場合 (ファイルを作成)
-			New-Item -Type File "$item"
+			New-Item -Type File "$path"
 		}
 	}
 }
