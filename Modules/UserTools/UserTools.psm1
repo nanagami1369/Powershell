417329156273a4720a60b7049a0a359fbf5c6fe3
nanagami1369 <nanagami1369@gmail.com>
2021-04-20 23:21:30 +0900

    Dockerから使われなくなったNoneコンテナを削除する関数を作成

--- Modules/UserTools/UserTools.psm1
+++ Modules/UserTools/UserTools.psm1
@@ -1,34 +1,40 @@
 #新しいモジュールを作る関数
 function New-ModuleSet {
-	Param(
-		[Parameter(Mandatory = $true)]
-		[ValidateNotNullOrEmpty()]
-		[String]
-		$ModuleName
-	)
-	$moduleDir = "$HOME\Documents\PowerShell\$ModuleName"
-	if (Test-Path $moduleDir) {
-		'This Module is Existing'
-		break 1
-	}
-	# モジュールフォルダーへ移動
-	Set-Location "$HOME\Documents\PowerShell\Modules"
-	# 作成
-	mkdir "$HOME\Documents\PowerShell\Modules\$ModuleName"
-	Set-Location "$ModuleName"
-	New-Item -Name "$ModuleName.psm1"
+    Param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ModuleName
+    )
+    $moduleDir = "$HOME\Documents\PowerShell\$ModuleName"
+    if (Test-Path $moduleDir) {
+        'This Module is Existing'
+        break 1
+    }
+    # モジュールフォルダーへ移動
+    Set-Location "$HOME\Documents\PowerShell\Modules"
+    # 作成
+    mkdir "$HOME\Documents\PowerShell\Modules\$ModuleName"
+    Set-Location "$ModuleName"
+    New-Item -Name "$ModuleName.psm1"
 }
 
 #指定したファイルorアプリの絶対パスを取得する関数
 function Search-Location {
-	[CmdletBinding()]
-	param (
-		[string]$FilePath = ''
-	)
-	if (Test-Path $FilePath) {
-		return (Get-Item $FilePath).FullName
-	}
-	return (where.exe youtube-dl)[-1]
+    [CmdletBinding()]
+    param (
+        [string]$FilePath = ''
+    )
+    if (Test-Path $FilePath) {
+        return (Get-Item $FilePath).FullName
+    }
+    return (where.exe youtube-dl)[-1]
 }
 
-Export-ModuleMember -Function New-ModuleSet , Search-Location
+#Dockerから使われなくなったNoneコンテナを削除する関数
+# 注意dockerをインストールしていないと使えない
+function Remove-NoneImagesForDocker {
+    docker image ls | ForEach-Object { if ($_ -match '<none>') { ($_ -split '\s\s*')[2] } } | ForEach-Object { docker image rm $_ }
+}
+
+Export-ModuleMember -Function New-ModuleSet , Search-Location, Remove-NoneImagesForDocker
